df701147f1c02d3216208e7407e2b592036ee124
ShenBao <shenbaoone@gmail.com>
2024-12-06 14:09:41 +0800

    feat: echarts-qiankun-demo

---
+++ examples/echarts-qiankun-demo/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Card id={1} />
+        <Card id={2} />
+        <Card id={4} />
+        <Card id={3} />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ examples/echarts-qiankun-demo/src/Card.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react";
+import { loadMicroApp, MicroApp } from "qiankun";
+
+const Card = ({ id }: { id: number }) => {
+  console.log(id);
+
+  const domRef = useRef<HTMLDivElement>(null);
+  const microAppRef = useRef<MicroApp>(null);
+
+  useEffect(() => {
+    if (domRef.current)
+      microAppRef.current = loadMicroApp({
+        name: "app1" + id,
+        entry: `http://192.168.1.9:8089/test${id}.html`,
+        container: domRef.current,
+        props: { brand: "qiankun" },
+      });
+  }, []);
+
+  return (
+    <div
+      style={{
+        display: "inline-block",
+        width: 600,
+        height: 400,
+      }}
+      className="card-wrapper"
+    >
+      <div ref={domRef} style={{ width: "100%", height: "100%" }}></div>
+    </div>
+  );
+};
+
+export default Card;
