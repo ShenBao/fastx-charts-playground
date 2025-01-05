a729e111d23a4f5c0af829c0b99c2a78d965c501
ShenBao <shenbaoone@gmail.com>
2025-01-05 22:04:33 +0800

    feat: add tsx

--- playground/vite.config.ts
+++ playground/vite.config.ts
@@ -2,9 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import monacoEditorEsmPlugin from "vite-plugin-monaco-editor-esm";
 
+const isDev = process.env.NODE_ENV === "development";
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/fastx-charts-playground/",
+  base: isDev ? "/" : "/fastx-charts-playground/",
   server: {
     proxy: {
       "/lib": {
